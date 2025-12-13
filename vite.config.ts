e8baeeb3652bac23fb4b11f7be6f7a0e9296f75a
Sameer0166 <sameerpasha0166@gmail.com>
2025-12-13 08:33:09 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // 'base' must be './' for GitHub Pages to find assets in the correct folder
-  base: './', 
-  define: {
-    // Safely shim process.env so the app doesn't crash in the browser
-    'process.env': JSON.stringify({}),
-  }
-});
+  base: '/ecosenseai/',
+})
